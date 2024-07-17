8f059bb80951dedfc094af272065f5ab2125a3d8
Surakit <surakitkunkayun@gmail.com>
2024-07-17 14:27:20 +0700

    feat: finished assignment

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,24 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+	let left = 0;
+    let right = students.length - 1;
+  
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (students[mid] === searchStudent) {
+            return mid;
+        } else if (students[mid] < searchStudent) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
 }
 
-// ตอบคำถามตรงนี้จ้า
+let students = ["Andrew", "Bobby", "Diana", "George", "Hannah", "Isaac", "Jasmine", "John"]
+let searchStudent = "John"
+
+console.log(findStudentIndex(students, searchStudent))
+
+
+// ตอบคำถามตรงนี้จ้า O(log n)
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,17 @@
 function findBookIndex(books, searchTitle) {
-  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let left = 0;
+  let right = books.length - 1;
+  
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (books[mid].title === searchTitle) {
+      return mid;
+  } else if (books[mid].title < searchTitle) {
+      left = mid + 1;
+  } else {
+      right = mid - 1;
+  }
 }
+return -1;
+}
+
